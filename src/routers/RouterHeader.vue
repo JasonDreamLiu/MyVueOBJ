a70057c309422e30896e894afe1232560dd24c17
Yumeng <xaiweiok@vip.qq.com>
2020-09-20 21:42:42 +0800

    设计Menu导航用于测试路由！

--- src/routers/RouterHeader.vue
+++ src/routers/RouterHeader.vue
@@ -1,32 +1,55 @@
 <template>
-  <Header>
+  <Header style="height: auto">
     <Row type="flex" :gutter="20">
-      <Col class="logo" span="4">
+      <Col class="logo" :span="4">
         <div>我是logo</div>
       </Col>
       <Col class="navMenu">
         <div>我是Menu</div>
+        <Menu :default-active="$route.path" mode="horizontal" router>
+          <template v-for="(item, index) in routes">
+            <MenuItem
+                v-if="item.children&&!item.children.length"
+                :key="index"
+                :index="item.path"
+            >
+              {{item.menuName}}
+            </MenuItem>
+
+          </template>
+        </Menu>
       </Col>
     </Row>
   </Header>
 </template>
 
 <script>
-import { Header, Row, Col } from 'element-ui';
+import {Header, Row, Col, Menu, MenuItem} from 'element-ui';
+
 export default {
   name: "RouterHeader",
-  components: { Header, Row, Col },
+  components: {Header, Row, Col, Menu, MenuItem},
+  data:function (){
+    return {
+      routes:[]
+    }
+  },
+  mounted: function () {
+    console.log(this.$router.options.routes);
+    this.routes = this.$router.options.routes;
+  }
 }
 </script>
 
 <style scoped>
-.logo{
+.logo {
   background: #409EFF;
 }
-.navMenu{
+
+.navMenu {
   background: #67c23a;
 }
-.logo,.navMenu{
-  height: 64px;
+
+.logo, .navMenu {
 }
 </style>
--- src/routers/router.js
+++ src/routers/router.js
@@ -5,6 +5,8 @@ import RouterHeader from "./RouterHeader";
 import RouterIndex from "../components/Index";
 import RouterBottom from "./RouterBottom";
 
+import My from "../components/My";
+
 import HelloWorld from "../components/HelloWorld";
 
 Vue.use(VueRouter);
@@ -12,14 +14,26 @@ Vue.use(VueRouter);
 export default new VueRouter({
     routes:[
         {
+            isMenu:true,
+            menuName:'首页',
             path: '/',
             components:{
                 default:RouterIndex,
                 header:RouterHeader,
                 bottom:RouterBottom
-            }
+            },
+            children:[
+                {
+                    isMenu:true,
+                    menuName:'简介',
+                    path:'/my',
+                    component:My
+                }
+            ]
         },
         {
+            isMenu: true,
+            menuName: '哈喽！',
             path:'/hello',
             component: HelloWorld
         }
